1c4e8e903f05362de04583bb0006fd790eca9591
StephanieCobble <cobble.stephanie@gmail.com>
2022-03-15 13:08:18 -0700

    added exported JSON from Postman

--- products/views.py
+++ products/views.py
@@ -23,11 +23,6 @@ def products_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-
-
-
-
-
 @api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
