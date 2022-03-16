b2a909d258b3733bdc36d998781ad4cdb79f0790
StephanieCobble <cobble.stephanie@gmail.com>
2022-03-16 08:56:35 -0700

    updated views.py and urls.py to reflect List and Detail classes.

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns #added import
 from .import views
 
+# urlpatterns = [
+#     path('', views.products_list),
+#     path('<int:pk>/', views.product_detail),
+# ]
+
+
 urlpatterns = [
-    path('', views.products_list),
-    path('<int:pk>/', views.product_detail),
+    path('', views.ProductList.as_view()),
+    path('<int:pk>/', views.ProductDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
