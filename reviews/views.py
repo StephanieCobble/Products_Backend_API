07b363e063ec94ae847ffce0c7e6d5af712c9198
StephanieCobble <cobble.stephanie@gmail.com>
2022-03-16 12:18:36 -0700

    copied over a modified version of the views.py to reflect reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import Http404 
+from rest_framework.views import APIView  
+
+from rest_framework.response import Response  #
+from .serializers import ReviewSerializer  #
+from .models import Review  #
+from rest_framework import status  #
+
+
+class ReviewList(APIView):
+    def get(self, request, format=None):
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ReviewSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ReviewDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Review.objects.get(pk=pk)    #need fk?
+        except Review.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        review = self.get_object(pk)
+        serializer = ReviewSerializer(review)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        review = self.get_object(pk)
+        serializer = ReviewSerializer(review, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, pk, format=None):
+        review = self.get_object(pk)
+        review.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
